9c7e3a8811e9736370ce5cd6a748bd79f5cbf6ea
khaiyra <ayindekhairat@gmail.com>
2022-01-24 12:05:41 +0100

    new commit

--- week1_assignment/query.sql
+++ week1_assignment/query.sql
@@ -1,44 +1,50 @@
+-- Question 3
 -- How many taxi trips were there on January 15?
 SELECT
-COUNT(1)
+  CAST(tpep_pickup_datetime),
+  COUNT(*)  
 FROM
-	yellow_taxi_trips
-WHERE 
-	tpep_pickup_datetime = '2021-01-05 00:00:00'
-AND
-	tpep_dropoff_datetime = '2021-01-05 23:59:00';
+  yellow_taxi_trips
+WHERE
+    CAST(tpep_pickup_datetime) = '2021-01-05'
+GROUP BY
+  CAST(tpep_pickup_datetime);
 
-
+-- Question 4
 -- On which day it was the largest tip in January?
-SELECT tpep_pickup_datetime, MAX(Tip_amount) AS "tip"
+SELECT 
+    tpep_pickup_datetime,
+    MAX(Tip_amount) AS 'tip'
 FROM
 	yellow_taxi_trips
 GROUP BY (1)
-ORDER BY "tip" DESC;
+ORDER BY 'tip' DESC;
 
+-- Question 5
 -- What was the most popular destination for passengers picked up in central park on January 14?
 SELECT
 	tpep_pickup_datetime,
-	COUNT(zdo."Zone") AS "zone_count",
-	zpu."Zone" AS "pickup_loc",
-	zdo."Zone" AS "dropoff_loc"
+	COUNT(zdo.'Zone') AS 'zone_count',
+	zpu.'Zone' AS 'pickup_loc',
+	zdo.'Zone' AS 'dropoff_loc'
 FROM 
 	yellow_taxi_trips t 
 	JOIN zones zpu
-    ON t."PULocationID" = zpu."LocationID"
+    ON t.'PULocationID' = zpu.'LocationID'
     JOIN zones zdo
-    ON t."DOLocationID" = zdo."LocationID"
+    ON t.'DOLocationID' = zdo.'LocationID'
 WHERE
-	zpu."Zone" = 'Central Park' AND tpep_pickup_datetime = '2021-01-14 16:00:44'
+	zpu.'Zone' = 'Central Park' AND CAST(tpep_pickup_datetime) = '2021-01-14'
 GROUP BY
 	1,3, 4
 ORDER BY
-	"zone_count" DESC, 1 ASC;
+	zone_count DESC, 1 ASC;
 
+-- Question 6
 -- What's the pickup-dropoff pair with the largest average price for a ride (calculated based on total_amount)?
 SELECT
-	AVG(total_amount) AS "price",
-	CONCAT(zpu."Zone", ' / ', zdo."Zone") AS "pickup_dropoff" 
+	AVG(total_amount) AS 'price',
+	CONCAT(zpu.'Zone', ' / ', zdo.'Zone') AS 'pickup_dropoff'
 
 FROM
     yellow_taxi_trips t,
@@ -46,18 +52,8 @@ FROM
 	zones zdo
  
 WHERE
-	t."PULocationID" = zpu."LocationID" AND
-	t."DOLocationID" = zdo."LocationID"
+	t.'PULocationID' = zpu.'LocationID' AND
+	t.'DOLocationID' = zdo.'LocationID'
 GROUP BY (2)
 ORDER BY
-  "price" DESC;
-  
-
-SELECT
-  CAST(tpep_pickup_datetime AS DATE) AS "DAY",
-  COUNT(*) AS "count" 
-FROM
-  yellow_taxi_trips t
-GROUP BY
-  CAST(tpep_pickup_datetime AS DATE)
-ORDER BY "DAY" ASC;
+  'price' DESC;
